8290852f9389238b22ef2248733c053bda482960
Elton Santos <eltonaxl@hotmail.com>
2022-06-21 00:00:47 -0300

    Add bootstrap

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
-
+const notesRoutes = require('./routes/notes');
 
 const app = express();
 const port = 8000;
@@ -9,6 +9,9 @@ const port = 8000;
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/notes', notesRoutes);
 
 app.get('/', (req, res) => {
   res.render('home')
