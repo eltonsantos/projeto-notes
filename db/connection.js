76944032518caef1fca5190cdfdc81cc49782122
Elton Santos <eltonaxl@hotmail.com>
2022-06-26 15:37:33 -0300

    Connection with MongoDB Atlas

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,8 @@
 const {MongoClient} = require('mongodb');
-const url = "mongodb://localhost:27017/notesDb";
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const url = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.wh1wr.mongodb.net/Notes?retryWrites=true&w=majority`
+//const url = "mongodb://localhost:27017/notesDb";
 
 let _db;
 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Configurações
+require('dotenv').config();
 const express = require('express');
 const exphbs  = require('express-handlebars');
 const bodyParser = require('body-parser')
@@ -15,28 +16,29 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use('/notes', notesRoutes);
-
 // Rotas
 app.get('/', function(req, res) {
-
+  
   (async() => {
-
+    
     const notes = await db.getDb().db().collection('notes').find({}).toArray() 
-
+    
     // console.log(notes) 
-
+    
     res.render('home', {notes});
-
+    
   })()
-    .catch(err => console.log(err))
+  .catch(err => console.log(err))
   
 });
+
+app.use('/notes', notesRoutes);
 
 db.initDb((err, db) => {
   if(err) {
     console.log(err);
   } else {
+    console.log('O banco conectou com sucesso!');
     app.listen(port, () => {
       console.log(`Projeto rodando na porta:${port}`)
     })
